2ce3fd63c154786abaa60a55aae253249d59c48f
dimasaditt <aditcraftyt@gmail.com>
2024-10-04 10:20:08 +0700

    modifikasi 2

--- pemilihan2Percobaan106.java
+++ pemilihan2Percobaan106.java
@@ -9,10 +9,14 @@ public class pemilihan2Percobaan106 {
         tahun = input06.nextInt();
 
         if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0) {
-                System.out.println("Tahun Kabisat");
+            if ((tahun % 100) == 0  ) {
+                if ((tahun % 400) == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
             } else {
-                System.out.println("Bukan tahun kabisat");
+                System.out.println("Tahun Kabisat");
             }
         } else {
             System.out.println("Bukan tahun kabisat");
