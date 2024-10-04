15bbb8b016fccbca51911adc82246fb8cb3c93c5
dimasaditt <aditcraftyt@gmail.com>
2024-10-04 10:10:06 +0700

    Add pemilihan 2 percobaan 1

---
+++ pemilihan2Percobaan106.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class pemilihan2Percobaan106 {
+    public static void main(String[] args) {
+        Scanner input06 = new Scanner(System.in);
+        int tahun;
+
+        System.out.print("Masukkan Tahun : ");
+        tahun = input06.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0) {
+                System.out.println("Tahun Kabisat");
+            }
+        } else {
+            System.out.println("Bukan tahun kabisat");
+        }
+    }
+}
